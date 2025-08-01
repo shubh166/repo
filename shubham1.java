69c31b17ca210a76a44bde900bb107db5fabf0e9
shubh166 <shubh14898@gmail.com>
2025-08-01 11:54:11 +0530

    Update shubham1.java

--- shubham1.java
+++ shubham1.java
@@ -1,9 +1,14 @@
 class shubham1 {
   public static void main(String[] args){
-    login(2,3);
+    login(20,30);
+    login("Shubham", " can build java program");
 }
   static void login(int a, int b){
     int sum=a+b;
     System.out.println("Sum is: "+sum);
+}
+  static void String(String a, String b){
+    String sum = a+b;
+    System.out.println("Sum is :"+sum);
 }
 }
